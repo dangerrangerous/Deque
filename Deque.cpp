f826dcf743b726d715ec0091043ed1b9b9adc01d
DangerRangerous <dangerousdevops@gmail.com>
2017-05-30 18:45:28 -0700

    initial commit

---
+++ Deque.cpp
@@ -0,0 +1,24 @@
+// Deque.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+/*
+1) EnqueueFront 
+2) DequeFront 
+3) EnqueueBack 
+4) DequeBack 
+5) PeekFront 
+6) PeekBack 
+7) Any other operators that you feel are useful, especially in the implementation of the six listed operators. 
+8) Utilize an instance of the Deque class in the main function so that all operators are tested.
+
+Use an instance of the DoublyLinkedList class as the fundamental structure to hold Deque values.
+
+*/
+
+int main()
+{
+    return 0;
+}
+
---
+++ DoublyLinkedList.h
@@ -0,0 +1,24 @@
+// DoublyLinkedList.h
+// Brian Keppinger
+
+#pragma once
+
+using namespace std;
+
+struct Node
+{
+
+};
+
+class DoublyLinkedList
+{
+public:
+	DoublyLinkedList(void);
+	~DoublyLinkedList(void);
+
+	void DoublyLinkedList::DestroyList();
+	Node* GetFirst();
+	void 
+
+private:
+};
