0b4a4069b53c0a8b46d318e7b11890d1fc1d57e6
DangerRangerous <dangerousdevops@gmail.com>
2017-05-30 18:55:59 -0700

    added more operators, gym time.

--- DoublyLinkedList.h
+++ DoublyLinkedList.h
@@ -7,7 +7,9 @@ using namespace std;
 
 struct Node
 {
-
+	int data;
+	Node* previous;
+	Node* next;
 };
 
 class DoublyLinkedList
@@ -18,7 +20,19 @@ public:
 
 	void DoublyLinkedList::DestroyList();
 	Node* GetFirst();
-	void 
+	void DoublyLinkedList::InitializeList();
+	
+	void DoublyLinkedList::InsertFirst(int);
+	void DoublyLinkedList::InsertLast(int);
+	bool DoublyLinkedList::IsEmpty();
+	
+	void DoublyLinkedList::DeleteFirst();
+	void DoublyLinkedList::DeleteLast();
+
+	void DoublyLinkedList::InsertNode(Node* inNode);
+	int DoublyLinkedList::NodeCount(DoublyLinkedList* inList);
 
 private:
+	Node* first;
+	Node* last;
 };
